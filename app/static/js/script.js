a86eeef3e6bc990a22af5bd315289d10136044e3
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-06-27 15:34:24 +0700

    Update: function javascript

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -24,49 +24,49 @@ dropArea.addEventListener("dragleave", () => {
 dropArea.addEventListener("drop", (event) => {
     event.preventDefault();
     file = event.dataTransfer.files[0];
+    upload();
+});
+
+function upload(){
     let fileType = file.type;
     let validExt = ["image/jpeg", "image/jpg", "image/png"];
     if (validExt.includes(fileType)){
-        // alert("OK");
-        upload();
+        let fileForm = new FormData();
+        if (file){
+            fileForm.append('file', file);
+        }
+        // console.log(file);
+        // let fileForm = {image: file};
+        // console.log(fileForm);
+        $.ajax({
+            method: 'POST',
+            url: '/upload',
+            data: fileForm,
+            processData: false,
+            cache: false,
+            contentType: false,
+            success: function (response){
+                if (response){
+                    // console.log(response);
+                    let bytestring = response['status'];
+                    let image = 'data:image/jpeg;base64,' + bytestring.split('\'')[1];
+                    imgResult.src = image;
+                }
+                else{
+                    console.log('No response');
+                    alert('No response');
+                }
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+        let resultBox = document.querySelector("#resultBox");
+        resultBox.classList.remove("d-none");
+        dropArea.classList.remove("active");
     }
     else{
         alert("Vui lòng tải lên file ảnh");
         dropArea.classList.remove("active");
     }
-});
-
-function upload(){
-    let fileForm = new FormData();
-    if (file){
-        fileForm.append('file', file);
-    }
-    console.log(file);
-    // let fileForm = {image: file};
-    console.log(fileForm);
-    $.ajax({
-        method: 'POST',
-        url: '/upload',
-        data: fileForm,
-        processData: false,
-        cache: false,
-        contentType: false,
-        success: function (response){
-            if (response){
-                console.log(response);
-                let bytestring = response['status']
-                let image = 'data:image/jpeg;base64,' + bytestring.split('\'')[1]
-                imgResult.src = image
-            }
-            else{
-                console.log('ko nhan dc phan hoi');
-            }
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
-    let resultBox = document.querySelector("#resultBox");
-    resultBox.classList.remove("d-none");
-    dropArea.classList.remove("active");
 }
