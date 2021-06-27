4ba8a3a3c919c7c3dfda90b645489d11750744ae
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-06-27 15:34:59 +0700

    Update: work well flask app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file, jsonify
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, jsonify
 import io
 import base64
-from PIL import Image
+from detect import *
 
 app = Flask(__name__)
 
@@ -18,19 +15,19 @@ def index():
 def upload():
     if request.method == 'POST':
         if 'file' not in request.files:
-            print('ko co file')
+            print('File was not found!')
         else:
-            print('co file')
+            print('File was found!')
             # Receive image
             img = request.files.get('file')
-            img_ext = img.filename.split('.')[-1].upper()
-            print(img_ext)
+            # img_ext = img.filename.split('.')[-1].upper()
+            # print(img_ext)
             img = img.read()
             numpy_img = np.frombuffer(img, np.uint8)
             img = cv2.imdecode(numpy_img, cv2.IMREAD_COLOR)
-            print(img.shape[0], img.shape[1])
-            plt.imshow(img)
-            plt.show()
+            # print(img.shape[0], img.shape[1])
+            # Process image
+            img = detect(img)
             # Send image
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(img.astype("uint8"))
@@ -39,12 +36,8 @@ def upload():
             raw_bytes.seek(0)
             img_base64 = base64.b64encode(raw_bytes.read())
             return jsonify({'status': str(img_base64)})
-
-            # return send_file(img, attachment_filename='logo.png', mimetype='image/png')
-        # return 'oke'
-    return 'not post'
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run()
-
