7bee47c675c26c6b547c0f7df6368df0de7ac50b
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-05 23:18:15 +0700

    Feature: Add yolo detection in app

--- app/app.py
+++ app/app.py
@@ -2,17 +2,24 @@ from flask import Flask, render_template, request, jsonify
 import io
 import base64
 from detect import *
+from yolo_python import yolo_detect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name_model='HOG and SVM')
+
+
+@app.route("/yolo")
+def yolo():
+    return render_template('index.html', name_model='Yolov4')
 
 
 @app.route("/upload", methods=['POST'])
 def upload():
+    name_model = str(request.referrer).split('/')[-1]
     if request.method == 'POST':
         if 'file' not in request.files:
             print('File was not found!')
@@ -27,7 +34,11 @@ def upload():
             img = cv2.imdecode(numpy_img, cv2.IMREAD_COLOR)
             # print(img.shape[0], img.shape[1])
             # Process image
-            img = detect(img)
+            if name_model == "yolo":
+                img = yolo_detect(img)
+            else:
+                img = detect(img, 'models_well.dat',
+                            score=0.4, thresh=0.45, img_w=300, img_h=150, downscale=1.5, step_size=(6, 6))
             # Send image
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(img.astype("uint8"))
